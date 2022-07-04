43025da8f84db5c8cbe7d274b01abee3786b73c1
hanbinChae <hanbin2043@gmail.com>
2022-07-04 10:27:04 +0900

    2차

---
+++ COS_Pro2/2차.py
@@ -0,0 +1,18 @@
+price = 96900
+grade = "S"
+
+def solution(price,grade):
+    answer = price
+    membership = {"S":5/100,"G":10/100,"V":15/100}
+    for m in membership.keys():
+        if grade == m:
+            price *= membership[m]
+            answer -= price
+    return int(answer)
+
+
+
+
+
+ret = solution(price,grade);
+print(ret)
