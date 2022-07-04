6dfc0889699f67d7a6f4fb26842f5ffbea4a058d
hanbinChae <hanbin2043@gmail.com>
2022-07-04 10:10:47 +0900

    1차

--- COS_Pro2/1차.py
+++ COS_Pro2/1차.py
@@ -4,7 +4,7 @@ def solution(shirt_size):
     answer = []
     check = {"XS":0,"S":0,"M":0,"L":0,"XL":0,"XXL":0}
     for i in shirt_size:
-        for j in check:
+        for j in check.keys():
             if i == j:
                 check[j]+=1
     for i in check.values():
