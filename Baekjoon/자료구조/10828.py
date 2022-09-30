f9c19d58ab25b905960badbb58063441f0792c27
hanbinChae <hanbin2043@gmail.com>
2022-10-01 00:11:43 +0900

    10/1

--- Baekjoon/자료구조/10828.py
+++ Baekjoon/자료구조/10828.py
@@ -1,23 +1,38 @@
+import sys
 stack = []
 
+def pop_(stack):
+    if stack:
+        return stack.pop()
+    else:
+        return -1
+def empty_(stack):
+    if not stack:
+        return 1
+    else:
+        return 0
+
+def top_(stack):
+    if stack:
+        return (stack[-1])
+    else:
+        return (-1)
+
 for _ in range(int(input())):
-    x = input()
-    if 'push' in x:
-        stack.append(x[-1])
-    elif 'pop' in x:
-        if stack:
-            print(stack.pop())
-        else:
-            print(-1)
-    elif 'size' in x:
+    x = sys.stdin.readline().split() 
+    # 입출력 속도 비교 : sys.stdin.readline > raw_input() > input()
+    if x[0] == 'push':
+        stack.append(x[1])
+    elif x[0] =='pop':
+        r = pop_(stack)
+        print(r)
+    elif x[0] == 'size':
         print(len(stack))
-    elif 'empty' in x:
-        if not stack:
-            print(1)
-        else:
-            print(0)
-    elif 'top' in x:
-        if stack:
-            print(stack[-1])
-        else:
-            print(-1)
+    elif x[0] =='empty':
+        r = empty_(stack)
+        print(r)
+    elif x[0] =='top':
+        r = top_(stack)
+        print(r)
+    else:
+        pass;     
