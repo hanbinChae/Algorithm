7c8d3f72740139cff9d74a8d3b70c0f47f38f6cd
hanbinChae <hanbin2043@gmail.com>
2022-10-28 23:47:47 +0900

    10/28

--- Baekjoon/자료구조/10799 미해결.py
+++ Baekjoon/자료구조/10799 미해결.py
@@ -1,1 +1,18 @@
 # 쇠막대기
+
+bar_size = list(input())
+stack = []
+result = 0
+
+for i in range(len(bar_size)):
+    if bar_size[i] == '(':
+        stack.append('(')
+    else:
+        if bar_size[i-1] =='(':
+            stack.pop()
+            result += len(stack)
+        else:
+            stack.pop()
+            result+=1
+print(result)
+        
